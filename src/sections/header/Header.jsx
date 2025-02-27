4f71fb8a8e4c1fe830b0a7e119fd61c3bd46e976
Kenneth Imade <kenneth.imade@yahoo.com>
2025-02-27 23:51:05 +0000

    feat: setup header view for mobild and desktop

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,8 +1,31 @@
+import data from './data'
+import HeaderImage from '../../assets/header.jpg'
 import './header.css'
 
 const Header = () => {
   return (
-    <header id='header'><h2>Header</h2></header>
+    <header id='header'>
+      <div className="container header__container">
+        <div className="header__profile">
+          <img src={HeaderImage} alt="Portrait" />
+        </div>
+        <h3>Kenneth Imade</h3>
+        <p>
+          I build data-driven applications that turn insights into action. With expertise in software engineering,
+          data science, and web development, I create scalable solutions that empower businesses and users.
+        </p>
+        <div className="header__cta">
+          <a href="#contact" className='btn primary'>Let's Talk</a>
+          <a href="#portfolio" className='btn light'>My Work</a>
+        </div>
+        <div className="header__socials">
+          {
+            data.map(item => <a key={item.id} href={item.link} target='_blank' 
+              rel='noopener noreferer'>{item.icon}</a>)
+          }
+        </div>
+      </div>
+    </header>
   )
 }
 
---
+++ src/sections/header/data.js
@@ -0,0 +1,9 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+const data = [
+    {id: 1, link: 'https://www.linkedin.com/in/kenneth-imade/', icon: <FaLinkedin />},
+    {id: 2, link: 'https://github.com/KenImade', icon: <FaGithub />},
+]
+
+export default data;
