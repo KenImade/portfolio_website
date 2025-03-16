4af6a07fa96ee0aac8994b6f90993d5110b8b626
Kenneth Imade <kenneth.imade@yahoo.com>
2025-03-16 22:42:19 +0000

    added stock market project

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -1,4 +1,5 @@
 import realEstateDashboardImage from '../../assets/real_estate_dashboard.jpg';
+import stockMarketDagImage from '../../assets/stockMarketDAG.png';
 
 const data = [
     {
@@ -10,6 +11,16 @@ const data = [
         tools: ['Looker Studio','Google Cloud','SQL', 'Python', 'Airflow', 'dbt', 'Docker', 'Terraform'],
         demo: "https://lookerstudio.google.com/reporting/0cce3cd0-a312-49ee-b4e9-5439192078ec",
         github: "https://github.com/KenImade/real_estate_dashboard"
+    },
+    {
+        id: 2,
+        category: 'data engineering',
+        image: stockMarketDagImage,
+        title: "Stock Price Dashboard",
+        desc: "This project implements an Apache Airflow DAG that automates the analysis of stock data. The pipeline is designed to fetch stock prices for a given symbol, process and format the data, and finally load it into a data warehouse for further analysis. It leverages various Airflow operators and integrations including Python tasks, Docker containers, and Slack notifications.",
+        tools: ['Airflow','Metabase','PostgreSQL', 'Python', 'Spark', 'Docker', 'Minio'],
+        demo: "https://github.com/KenImade/stock_market_pipeline",
+        github: "https://github.com/KenImade/stock_market_pipeline"
     },
 ]
 
